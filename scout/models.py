9d06a337dc35811068741148f5c42983eda65912
Your Name <you@example.com>
2022-04-03 12:09:06 -0400

    working pit scouting, match scouting nonfunctional

--- scout/models.py
+++ scout/models.py
@@ -14,17 +14,17 @@ class Team(models.Model):
     
 
 class Robot(models.Model):
-    NONE = 0
-    LOW = 1
-    HIGH = 2
-    BOTH = 3
+    NONE = 'None'
+    LOW = 'Low Goal'
+    HIGH = 'High Goal'
+    BOTH = 'Both Goals'
 
-    LOWBAR = 4
-    MIDBAR = 6
-    HIGHBAR = 10
-    TRAVERSE = 15
+    LOWBAR = 'Low Bar'
+    MIDBAR = 'Mid Bar'
+    HIGHBAR = 'High Bar'
+    TRAVERSE = 'Traversal Bar'
 
-    frc_team = models.OneToOneField(Team, related_name='robot',null=False, on_delete=models.CASCADE)
+    frc_team = models.OneToOneField(Team, related_name='robot', null=True, on_delete=models.CASCADE)
 
     tele_goal = ((LOW, 'Low'), (HIGH, 'High'), (BOTH, 'Both'), (NONE, 'None'))
     climb_level = ((LOWBAR, 'Low Bar'), (MIDBAR, 'Middle Bar'), (HIGHBAR, 'High Bar'), (TRAVERSE, 'Traversal Bar'), (NONE, 'No Climb'))
@@ -35,40 +35,40 @@ class Robot(models.Model):
     # drive train type
     drive_type = models.CharField(max_length=45)
     # goal (low/high/both)
-    target = models.PositiveSmallIntegerField(choices=tele_goal)
+    target = models.CharField(choices=tele_goal, max_length=11)
 
     # climb bar (none, low, mid, high, traverse)
-    climb = models.PositiveSmallIntegerField(choices=climb_level)
+    climb = models.CharField(choices=climb_level, max_length=13)
 
     def get_absolute_url(self):
         return reverse('scout:team_summary', kwargs={'number': self.frc_team.number})
     
     def __str__(self):
-        return f"{self.frc_team}"
+        return f"Expected Auto Score: {self.auto_points}, Drive Train: {self.drive_type}, Preferred Target: {self.target}, Climb Height: {self.climb}"
 
 
 class MatchResult(models.Model):
-    NONE = 0
+    NONE = 'None'
     
-    AUTOLOW = 2
-    AUTOHIGH = 4
+    AUTOLOW = 'Low Goal'
+    AUTOHIGH = 'High Goal'
 
-    TELELOW = 1
-    TELEHIGH = 2
-    TELECLOSE = 1
-    TELEFAR = 2
-    TELEANYWHERE = 3
+    TELELOW = 'Low Goal'
+    TELEHIGH = 'High Goal'
+    TELECLOSE = 'Close'
+    TELEFAR = 'Far'
+    TELEANYWHERE = 'Anywhere'
 
-    CLIMBLOW = 4
-    CLIMBMID = 6
-    CLIMBHIGH = 10
-    TRAVERSE = 15
+    CLIMBLOW = 'Low Bar'
+    CLIMBMID = 'Mid Bar'
+    CLIMBHIGH = 'High Bar'
+    TRAVERSE = 'Traversal Bar'
 
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
+    ONE = 'Really Bad'
+    TWO = 'Bad'
+    THREE = 'Moderate'
+    FOUR = 'Good'
+    FIVE = 'Really Good'
     
     auto_target_field = ((AUTOHIGH, 'High Goal'), (AUTOLOW, 'Low Goal'), (NONE, 'No Auto'))
     tele_target_field = ((TELEHIGH, 'High Goal'), (TELELOW, 'Low Goal'), (NONE, 'No Teleop Shooting'))
@@ -76,28 +76,32 @@ class MatchResult(models.Model):
     climb_height_field = ((CLIMBLOW, 'Low Bar'), (CLIMBMID, 'Mid Bar'), (CLIMBHIGH, 'High Bar'), (TRAVERSE, 'Traversal Bar'), (NONE, 'No Climb'))
     driver_rating_field = ((ONE, 'Really Bad'), (TWO, 'Bad'), (THREE, 'Moderate'), (FOUR, 'Good'), (FIVE, 'Really Good'))
 
-    frc_team = models.ForeignKey(Team, related_name='matches',null=True , on_delete=models.CASCADE)
+    frc_team = models.ForeignKey(Team, related_name='matches', null=True , on_delete=models.CASCADE)
 
+    match_number = models.IntegerField(default=0)
     # Auto taxi (T/F)
     auto_taxi = models.BooleanField(default=False)
     # Auto ball count (int field)
     auto_ball = models.IntegerField(default=0, max_length=1)
     # Auto target (high/low/none)
-    auto_target = models.SmallIntegerField(choices=auto_target_field, default=NONE)
+    auto_target = models.CharField(choices=auto_target_field, default=NONE, max_length=13)
     
     # Teleop target (high/low/both/none)
-    tele_target = models.SmallIntegerField(choices=tele_target_field, default=NONE)
+    tele_target = models.CharField(choices=tele_target_field, default=NONE, max_length=13)
     # Teleop cargo count (int field)
     tele_cargo = models.IntegerField(default=0, max_length=2)
     # Teleop shot distances (close only, far only, anywhere, no shooting)
-    tele_distance = models.SmallIntegerField(choices=tele_distance_field, default=NONE)
+    tele_distance = models.CharField(choices=tele_distance_field, default=NONE, max_length=13)
 
     # Climb location (low, mid, high, traverse, none)
-    climb_height = models.SmallIntegerField(choices=climb_height_field, default=NONE)
+    climb_height = models.CharField(choices=climb_height_field, default=NONE, max_length=13)
     # climb time (int field)
     climb_time = models.IntegerField(default=0, max_length=2)
 
     # Driver skill rating (1 - 5)
-    driver_rating = models.SmallIntegerField(choices=driver_rating_field, default=ONE)
+    driver_rating = models.CharField(choices=driver_rating_field, default=ONE, max_length=13)
     # Addtitonal Comments
-    comments = models.CharField(max_length=250)
+    comments = models.CharField(max_length=250, default='No comments')
+
+    def get_absolute_url(self):
+        return reverse('scout:team_summary', kwargs={'number': self.frc_team.number})
